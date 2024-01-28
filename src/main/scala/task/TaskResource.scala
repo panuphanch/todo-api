909ed17d57920d912d32dad782633cdf3af96c6d
Panuphan Chaimanee <c.panuphan@gmail.com>
2024-01-28 11:04:31 +0700

    Create todo api with test

--- src/main/scala/task/TaskResource.scala
+++ src/main/scala/task/TaskResource.scala
@@ -5,5 +5,19 @@ import com.twitter.finagle.http.Request
 import scala.collection.mutable.SortedMap
 
 class TaskResource extends Controller {
+	var todoList: SortedMap[Int, Todo] = SortedMap.empty[Int, Todo]
+
 	get("/ping") { _: Request => "pong" }
+
+	post("/todo") { todo: Todo => 
+		val item = todoList.lastOption
+		val id = item match {
+			case Some(value) => value._1 + 1
+			case None => 0
+		}
+		todoList.addOne(id -> Todo(id, todo.title))
+		response.created(s"created $id")
+	}
+
+	get("/todo") { _:Request => todoList.values.toList }
 }
